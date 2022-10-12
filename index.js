c3bcd4fcf64e6bea28b93e856a379fdd96c7e97d
JS <Jdavidsimanca10@gmail.com>
2022-10-12 18:02:31 -0500

    add heroku, needed config

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3002
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
